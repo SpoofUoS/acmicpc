3cdcbfec6dbc9b3a2302c4fcb9f63c054f105068
Spoof-UoS <74731011+Spoof-UoS@users.noreply.github.com>
2021-04-15 21:35:35 +0900

    2021.04.15.

--- 1074.c
+++ 1074.c
@@ -1,1 +1,18 @@
 #include<stdio.h>
+int calc(int x)
+{
+    int answer = 0;
+    int n = 1;
+    while(x) {
+        if(x&1) answer += n*n;
+        n <<= 1;
+        x >>= 1;
+    }
+    return answer;
+}
+
+int main(void){
+    int n, r, c;
+    scanf("%d %d %d",&n,&r,&c);
+    printf("%d",((calc(r)<<1)+calc(c)));
+}
